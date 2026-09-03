34a44b3bacad394d233e9cf8ec75f6fd9af35b28
naliling <3632823490@qq.com>
2026-09-03 21:30:27 +0800

    Update __init__.py

--- humanoid/services/__init__.py
+++ humanoid/services/__init__.py
@@ -1,1 +1,19 @@
+"""各领域服务。"""
 
+from __future__ import annotations
+
+from .energy import EnergyService
+from .mood import MoodService
+from .process import ProcessService
+from .schedule import ScheduleService
+from .social import SocialEnergyService
+from .weather import WeatherService
+
+__all__ = [
+    "EnergyService",
+    "MoodService",
+    "ProcessService",
+    "ScheduleService",
+    "SocialEnergyService",
+    "WeatherService",
+]
