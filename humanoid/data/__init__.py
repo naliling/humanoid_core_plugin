d4700a223c167526bb72e0031db7b24e0d77d418
naliling <3632823490@qq.com>
2026-09-03 21:31:52 +0800

    Enhance __init__.py with data imports and exports
    Added imports and updated __all__ for module exports.

--- humanoid/data/__init__.py
+++ humanoid/data/__init__.py
@@ -1,1 +1,21 @@
+"""纯数据表与其查询函数。"""
 
+from __future__ import annotations
+
+from .cities import CITY_TO_TIMEZONE, DEFAULT_CITY_PLACEHOLDER, lookup_timezone
+from .holidays import BUILTIN_HOLIDAYS, resolve_holiday
+from .mood_map import AFFECTION_MAP, generate_mood_tag, get_mood_label
+from .schedule_templates import FALLBACK_TEMPLATES, get_fallback_schedule
+
+__all__ = [
+    "AFFECTION_MAP",
+    "BUILTIN_HOLIDAYS",
+    "CITY_TO_TIMEZONE",
+    "DEFAULT_CITY_PLACEHOLDER",
+    "FALLBACK_TEMPLATES",
+    "generate_mood_tag",
+    "get_fallback_schedule",
+    "get_mood_label",
+    "lookup_timezone",
+    "resolve_holiday",
+]
